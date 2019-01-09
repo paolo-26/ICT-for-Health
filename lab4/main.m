8f43413e395c5dca56531c3b071c0cabf0f6db8e
paolo-26 <paolo.1995@ymail.com>
2019-01-09 10:01:53 +0100

    Add white spaces

--- lab4/main.m
+++ lab4/main.m
@@ -2,14 +2,14 @@ clear all
 close all
 %%
 rng('default');
-tstart=tic;
-Kquant=8;% number of quantization levels
-Nstates=8;% number of states in the HMM
-ktrain=[1,2,3,4,5,6,7];% indexes of patients for training
-ktest=[8,9,10];% indexes of patients for testing
-[hq,pq]=pre_process_data(Nstates,Kquant,ktrain);% generate the quantized signals
+tstart = tic;
+Kquant = 8;  % Number of quantization levels
+Nstates = 8;  % Number of states in the HMM
+ktrain = [1,2,3,4,5,6,7];  % Indexes of patients for training
+ktest = [8,9,10];  % Indexes of patients for testing
+[hq, pq] = pre_process_data(Nstates, Kquant, ktrain);  % Generate the
+                                                       % quantized signals
 telapsed = toc(tstart);
-disp(['first part, elapsed time ',num2str(telapsed),' s'])
+disp(['first part, elapsed time ', num2str(telapsed), ' s'])
 %% HMM training phase....
 %% HMM testing phase....
-
