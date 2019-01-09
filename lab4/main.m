aa09279cf67f3c81641787eb426439405737db3c
Paolo Grasso <paolo.1995@ymail.com>
2019-01-09 22:17:49 +0100

    Update file

--- lab4/main.m
+++ lab4/main.m
@@ -1,15 +1,17 @@
-clear all
-close all
-%%
+clear all; close all; clc
+
 rng('default');
 tstart = tic;
 Kquant = 8;  % Number of quantization levels
 Nstates = 8;  % Number of states in the HMM
 ktrain = [1,2,3,4,5,6,7];  % Indexes of patients for training
 ktest = [8,9,10];  % Indexes of patients for testing
-[hq, pq] = pre_process_data(Nstates, Kquant, ktrain);  % Generate the
+[hq, pq] = pre_process_data(Nstates, Kquant, ktrain);  % Generate the...
                                                        % quantized signals
 telapsed = toc(tstart);
 disp(['first part, elapsed time ', num2str(telapsed), ' s'])
 %% HMM training phase....
+
+plot([hq{1}])
+
 %% HMM testing phase....
