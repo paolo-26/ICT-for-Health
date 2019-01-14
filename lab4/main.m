01e91869ce40c086defa8176d1dde6f3fe6c4b03
Paolo Grasso <paolo.1995@ymail.com>
2019-01-14 19:36:07 +0100

    Add hmmtrain

--- lab4/main.m
+++ lab4/main.m
@@ -2,31 +2,66 @@ clear all; close all; clc
 format short;
 
 rng('default');
-tstart = tic;
-Kquant = 8;  % Number of quantization levels
-Nstates = 8;  % Number of states in the HMM
-ktrain = [1,2,3,4,5,6,7];  % Indexes of patients for training
-ktest = [8,9,10];  % Indexes of patients for testing
-[hq, pq] = pre_process_data(Nstates, Kquant, ktrain);  % Generate the...
+tStart = tic;
+nQuant = 8;  % Number of quantization levels
+nStates = 8;  % Number of states in the HMM
+kTrain = [1, 2, 3, 4, 5, 6, 7];  % Indexes of patients for training
+kTest = [8, 9, 10];  % Indexes of patients for testing
+[hq, pq] = pre_process_data(nStates, nQuant, kTrain);  % Generate the...
                                                        % quantized signals
-telapsed = toc(tstart);
-disp(['first part, elapsed time ', num2str(telapsed), ' s'])
+tElapsed = toc(tStart);
+disp(['first part, elapsed time ', num2str(tElapsed), ' s'])
+
 %% HMM training phase....
+TOL = 1e-3;  % Tolerance
+MAX_ITER = 200;  % Maximum number of iterations
 
 transGuess = rand(8,8);
-s = sum(transGuess(:,:)');
-for k = 1:8
-    transGuess(k,:) = transGuess(k,:)/s(k);
+% Normalize rows
+s = sum(transGuess, 2);
+for iRow = 1:8
+    transGuess(iRow,:) = transGuess(iRow,:) / s(iRow);
 end
 
 emisGuess = rand(8,8);
-s = sum(emisGuess(:,:)');
-for k = 1:8
-    emisGuess(k,:) = emisGuess(k,:)/s(k);
+% Normalize rows
+s = sum(emisGuess, 2);
+for iRow = 1:8
+    emisGuess(iRow,:) = emisGuess(iRow,:) / s(iRow);
 end
 
+% Health machine training
+[transH, emisH] = hmmtrain(hq(kTrain), transGuess, emisGuess,...
+                           'algorithm', 'baumwelch',...
+                           'tolerance', TOL,...
+                           'maxiterations', MAX_ITER);
 
-[trans, emis] = hmmtrain(hq(ktrain), transGuess, emisGuess);
+% Parkinson machine training
+[transP, emisP] = hmmtrain(pq(kTrain), transGuess, emisGuess,...
+                           'algorithm', 'baumwelch',...
+                           'tolerance', TOL,...
+                           'maxiterations', MAX_ITER);
+figure(1)
+pcolor(transH); colorbar
+axis square; title({'Transition matrix (training)', 'Healthy patients'})
+xlabel('state'); ylabel('state')
 
+figure(2)
+pcolor(emisH); colorbar
+axis square; title({'Emission matrix (training)', 'Healthy patients'})
+xlabel('state'); ylabel('state')
+
+figure(3)
+pcolor(transP); colorbar
+axis square; title({'Transition matrix (training)', 'Ill patients'})
+xlabel('state'); ylabel('state')
+
+figure(4)
+pcolor(emisP); colorbar
+axis square; title({'Emission matrix (training)', 'Ill patients'})
+xlabel('state'); ylabel('state')
 
 %% HMM testing phase....
+
+% PSTATES = hmmdecode(hq{8}, trans, emis);
+
